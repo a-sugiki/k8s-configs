b47b301fd9e0b908d3e7a3c283b0f88ad7ec0f06
Akiyoshi Sugiki <sugiki@iic.hokudai.ac.jp>
2022-04-12 17:18:49 +0900

    Initial files addtion

---
+++ install_dep_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# note: https://github.com/kubernetes-client/python/issues/1333
+pip3 install -Iv kubernetes==11.0.0
+
+pip3 install openshift
+ansible-galaxy collection install kubernetes.core
+
