0d0c9b85bb3398791382e4f2690ade3aa840052b
Akiyoshi Sugiki <sugiki@iic.hokudai.ac.jp>
2023-01-25 00:38:01 +0900

    A quick fix for version incompatiblity in install_dep_packages.sh

--- install_dep_packages.sh
+++ install_dep_packages.sh
@@ -2,6 +2,8 @@
 
 # note: https://github.com/kubernetes-client/python/issues/1333
 pip3 install -Iv kubernetes==11.0.0
-pip3 install openshift
+pip3 install -Iv openshift==0.11.2
 ansible-galaxy collection install kubernetes.core
 
+pip install -Iv jinja2==3.0.3
+pip install jinja2-base64-filters==0.1.4
